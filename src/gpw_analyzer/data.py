aaba815b004f60f46782045c0a9c4e76120b6af2
michailgames <michailgames@yahoo.com>
2017-05-14 15:52:29 +0200

    added casting to float in Candle constructor

--- src/gpw_analyzer/data.py
+++ src/gpw_analyzer/data.py
@@ -1,11 +1,11 @@
 class Candle(object):
 
     def __init__(self, opening, closing, minimum, maximum, volume):
-        self.opening = opening
-        self.closing = closing
-        self.min = minimum
-        self.max = maximum
-        self.volume = volume
+        self.opening = float(opening)
+        self.closing = float(closing)
+        self.min = float(minimum)
+        self.max = float(maximum)
+        self.volume = float(volume)
 
     def is_black(self):
         return self.opening > self.closing
--- src/gpw_analyzer/engulfings.py
+++ src/gpw_analyzer/engulfings.py
@@ -1,7 +1,3 @@
-def find_prosperity_engulfings(charts):
-    return filter(lambda chart: is_prosperity_engulfing(chart.candles), charts)
-
-
 def is_prosperity_engulfing(candles):
     if len(candles) < 2:
         return False
