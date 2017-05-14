5876e78837aaa33a803c9d9526204a8e34d6feb4
michailgames <michailgames@yahoo.com>
2017-05-14 21:52:52 +0200

    added minimum tresholds for double bottoms for more accurate results

--- src/gpw_analyzer/double_bottoms.py
+++ src/gpw_analyzer/double_bottoms.py
@@ -1,4 +1,5 @@
-def find_full_candle_breakthrough(candles, max_candles=50):
+def find_full_candle_breakthrough(
+        candles, max_candles=50, minimum_drop=0.05, max_bottoms_delta=0.03):
     effective_candles = candles[-max_candles:]
     if len(effective_candles) < 2:
         return None
@@ -18,9 +19,24 @@ def find_full_candle_breakthrough(candles, max_candles=50):
 
     middle_index, middle_max = max(
         enumerate([c.max for c in bottom_candles]), key=lambda p: p[1])
-    if middle_max > yesterday.closing:
-        left_candles = bottom_candles[1:middle_index]
-        right_candles = bottom_candles[middle_index + 1: -1]
-        return True
-    else:
+    if middle_max < yesterday.closing:
         return None
+
+    left_candles = bottom_candles[1:middle_index]
+    right_candles = bottom_candles[middle_index + 1: -1]
+    if not left_candles or not right_candles:
+        return None
+
+    left_bottom = min(c.min for c in left_candles)
+    right_bottom = min(c.min for c in right_candles)
+
+    max_bottom_value = (1 - minimum_drop) * middle_max
+    if left_bottom > max_bottom_value or right_bottom > max_bottom_value:
+        return None
+
+    lower_bottom = min(left_bottom, right_bottom)
+    higher_bottom = max(left_bottom, right_bottom)
+    if lower_bottom / higher_bottom < (1 - max_bottoms_delta):
+        return None
+
+    return True
